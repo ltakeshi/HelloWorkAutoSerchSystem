4aebbb2960c4364c6d8d880b7a10895688d4985e
ltakeshi <tuxtakeshi@gmail.com>
2012-01-26 11:12:32 +0900

    検索可能になった

--- hello.rb
+++ hello.rb
@@ -9,64 +9,69 @@
 require 'mechanize'
 require 'logger'
 require 'net/https'
+require 'hpricot'
 
 agent = Mechanize.new
-agent.log = Logger.new('hello.log')
+#agent.log = Logger.new('hello.log')
 
-#検索トップページ
+# 検索トップページ
 page = agent.get("https://www.hellowork.go.jp/servicef/130020.do?action=initDisp&screenId=130020")
-#puts agent.page.title
 agent.user_agent_alias = 'Linux Firefox'
-agent.ca_file = '/home/takeshi/hello/JPGov.pem'
-agent.cert = '/home/takeshi/hello/JPGov.pem'
 
+# 検索ページ
 agent.page.form_with(:name => 'mainForm'){|form|
-#求職登録有無
+# 求職登録有無
   form.radiobuttons_with(:name => 'kyushokuUmu')[0].check
   form['kyushokuNumber1'] = '34050'
   form['kyushokuNumber2'] = '1437913'
-#求人情報の種類
+# 求人情報の種類
   form.radiobuttons_with(:name => 'kyujinShurui')[0].check
-#賃金
+# 賃金
   form['gekkyuKagen'] = '180000'
   form.checkbox_with(:text => /手当等を含む/).check
-#希望する職種
+# 希望する職種
   form.field_with(:name => 'kiboShokushu'){|list|
     list.value = "A"
   }
-#都道府県／市区町村名
+# 都道府県／市区町村名
   form.field_with(:name => 'todofuken1'){|list|
-    list.value = "26" #京都
+    list.value = "26" # 京都
   }
   form.field_with(:name => 'todofuken2'){|list|
-    list.value = "27" #大阪
+    list.value = "27" # 大阪
   }
   form.field_with(:name => 'todofuken3'){|list|
-    list.value = "28" #兵庫
+    list.value = "28" # 兵庫
   }
   form.field_with(:name => 'todofuken4'){|list|
-    list.value = "33" #岡山
+    list.value = "33" # 岡山
   }
   form.field_with(:name => 'todofuken5'){|list|
-    list.value = "34" #広島
+    list.value = "34" # 広島
   }
-#年齢
+# 年齢
   form['nenrei'] = '26'
-#希望する産業
+# 希望する産業
   form.field_with(:name => 'kiboSangyo'){|list|
     list.value = "G"
   }
 
-#検索ボタン
-#  agent.page.forms[0].click_button # 詳細条件入力
-  agent.page.forms[1].click_button # 検索
+# 検索ボタン
+  form.click_button(form.button_with(:name => 'commonNextScreen')) # 詳細条件入力
+#  form.click_button(form.button_with(:name => 'commonSearch')) # 検索
 
-  puts agent.page.body
 }
 
-#puts agent.page.title
+# 詳細条件入力のページ
+agent.page.form_with(:name => 'mainForm'){|form|
 
-#agent.page.search('p')[0..10].each {|p|
-#  puts p.inner_text
-#  puts '____'
-#}
+# 検索ボタン
+#  form.click_button(form.button_with(:name => 'commonNextScreen')) # 基本条件の変更
+  form.click_button(form.button_with(:name => 'commonSearch')) # 検索
+}
+
+#puts agent.page.body
+html_doc = Nokogiri::HTML(agent.page.body)
+puts html_doc.xpath("/html/body/div/div/div[4]/div/form[2]/div[2]/div[2]/table")
+
+#puts html_doc
