1df6a040ad5e72b28adf2ee54828e27726d7287b
ltakeshi <tuxtakeshi@gmail.com>
2012-01-30 00:21:26 +0900

    ``週所定労働日数"まで実装。``時間外"からは明日。

--- hello.rb
+++ hello.rb
@@ -62,7 +62,6 @@ agent.page.form_with(:name => 'mainForm'){|form|
   form.field_with(:name => 'kiboShokushu'){|list|
     list.value = $config["base"]["kiboShokushu"]
   }
-
 # 都道府県／市区町村名
   form.field_with(:name => 'todofuken1'){|list|
     list.value = $config["base"]["todofuken1"]
@@ -90,7 +89,7 @@ agent.page.form_with(:name => 'mainForm'){|form|
   form['chiku5'] = $config["base"]["todofuken5_chiku"]
 
 # 年齢
-  form['nenrei'] =  $config["base"]["nenrei"]
+  form['nenrei'] = $config["base"]["nenrei"]
 
 # 新着求人
   if $config["base"]["shinchakuKyujin"] == 1
@@ -108,25 +107,141 @@ agent.page.form_with(:name => 'mainForm'){|form|
   else
     form.click_button(form.button_with(:name => 'commonSearch')) # 検索
   end
-  puts agent.page.body
+#  puts agent.page.body
 }
 
 if $config["base"]["syousai"] == 1
 # 詳細条件入力のページ
   agent.page.form_with(:name => 'mainForm'){|form|
-# 希望する職種
-#    form.field_with(:name => 'kiboShokushuDetail'){|list|
-#      list.value = "06" # 情報処理技術者
-#    }
+
+# 雇用形態欄
+    if $config["detail"]["koyoKeitai"].nil?
+    else
+      $config["detail"]["koyoKeitai"].each {|i|
+        form.checkbox_with(:name => 'koyoKeitai', :value => "#{i}").check
+      }
+    end
+
+# 契約更新の可能性欄
+    if $config["detail"]["keiyakuKoshin"] == 1
+      form.checkbox_with(:name => 'keiyakuKoshin').check
+    end
+
+# マイカー通勤欄
+    if $config["detail"]["myCarTsukin"] == 1
+      form.checkbox_with(:name => 'myCarTsukin').check
+    end
+
+# 希望する職種(詳細)欄
+    if $config["detail"]["kiboShokushuDetail"].nil?
+    else
+      form.field_with(:name => 'kiboShokushuDetail'){|list|
+        list.value = $config["detail"]["kiboShokushuDetail"]
+      }
+    end
+
+# フリーワード欄
+    if $config["detail"]["freeWordType"] == 0
+      form.radiobutton_with(:value => '0',:name => 'freeWordType').check
+    else $config["detail"]["freeWordType"] == 1
+      form.radiobutton_with(:value => '1',:name => 'freeWordType').check
+    end
+    
+    form["freeWord"] = $config["detail"]["freeWord"]
+
+    if $config["detail"]["freeWordRuigigo"] == 1
+      form.checkbox_with(:name => 'freeWordRuigigo').check
+    end
+
+# 加入保険欄
+    if $config["detail"]["kanyuHoken"] == 1
+      form.checkbox_with(:name => 'kanyuHoken').check
+    end
+
+# 住込欄
+    if $config["detail"]["sumikomi"] == 1
+      form.checkbox_with(:name => 'sumikomi').check
+    end
+
+# 入居可能住宅欄
+    if $config["detail"]["nyukyoKanou"].nil?
+    else
+    $config["detail"]["nyukyoKanou"].each {|i|
+        form.checkbox_with(:name => 'nyukyoKanou', :value => "#{i}").check
+      }
+    end
+
+# 利用可能な託児所欄
+    if $config["detail"]["takujijo"] == 1
+      form.checkbox_with(:name => 'takujijo').check
+    end
+
+# 賞与欄
+    if $config["detail"]["shoyo"] == 1
+      form.checkbox_with(:name => 'shoyo').check
+    end
+
+# 希望する休日
+    if $config["detail"]["kyujitsu"].nil?
+    else
+    $config["detail"]["kyujitsu"].each {|i|
+        form.checkbox_with(:name => 'kyujitsu', :value => "#{i}").check
+      }
+    end
+
+# 週休二日欄
+    if $config["detail"]["shukyuFutsuka"] == 0
+      form.radiobutton_with(:value => '0',:name => 'shukyuFutsuka').check
+    elsif $config["detail"]["shukyuFutsuka"] == 1
+      form.radiobutton_with(:value => '1',:name => 'shukyuFutsuka').check
+    elsif $config["detail"]["shukyuFutsuka"] == 2
+      form.radiobutton_with(:value => '2',:name => 'shukyuFutsuka').check
+    elsif $config["detail"]["shukyuFutsuka"] == 3
+      form.radiobutton_with(:value => '3',:name => 'shukyuFutsuka').check
+    else $config["detail"]["shukyuFutsuka"] == 4
+      form.radiobutton_with(:value => '4',:name => 'shukyuFutsuka').check
+    end
+    
+# 年間休日数欄
+    if $config["base"]["kyujinShurui"] == 1 or $config["base"]["kyujinShurui"] == 3
+      form["nenkanKyujitsu"] = $config["detail"]["nenkanKyujitsu"]
+    end
+
+# 週所定労働日数欄
+    if $config["base"]["kyujinShurui"] == 2
+      form["shoteiRodoNissuKagen"] = $config["detail"]["shoteiRodoNissuKagen"]
+    end
+
+# 時間外欄
+
+
+# 希望する就業時間欄
+
+
+# 転勤欄
+
+
+# 免許・資格欄
+
+
+# 学歴欄
+
+
+# 経験欄
+
+
+# 事業所名欄
+
+
 # 検索ボタン
 #  form.click_button(form.button_with(:name => 'commonNextScreen')) # 基本条件の変更
 
-# 希望する休日
-#    $config["detail"]["kyujitsu"].each {|i|
-#      form.checkbox_with(:name => 'kyujitsu', :value => '1').check
-#    }
+
+
+#    form.checkbox_with(:name => 'keiyakuKoshin').check
 
     form.click_button(form.button_with(:name => 'commonSearch')) # 検索
+    puts agent.page.body
   }
 end
 
