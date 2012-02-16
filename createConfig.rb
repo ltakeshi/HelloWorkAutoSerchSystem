97fb6f0ad981e0e9cd7fc8b14c4f267a43754ad1
ltakeshi <tuxtakeshi@gmail.com>
2012-02-16 23:59:15 +0900

    createConfigを試行錯誤中

--- createConfig.rb
+++ createConfig.rb
@@ -2,3 +2,19 @@
 
 require 'optparse'
 require 'yaml'
+
+hash = {
+  'base' => 
+  { 'name' => 'hoge',
+    'getPageNum' => '10',
+  },
+  'detail' =>
+  {
+     'keiyakuKoshin' => '1',
+  }
+}
+
+yaml = YAML.load(hash.to_yaml)
+
+p yaml.class
+p yaml["base"]["name"]
