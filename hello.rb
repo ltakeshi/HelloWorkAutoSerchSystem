96ad7c4d9a2a4e9a5eb24421a9b1550a9dd9328f
ltakeshi <tuxtakeshi@gmail.com>
2012-02-24 15:24:30 +0900

    出力するHTMLファイルの更なる整形 RSS生成要のデータを取得し始めた

--- hello.rb
+++ hello.rb
@@ -326,7 +326,7 @@ open(FILENAME1,"a"){|f|
   html_doc = Nokogiri::HTML(agent.page.body)
   html_doc.xpath("/html/body/div/div/div[4]/div/form[2]/div[2]/div[2]/table/tr[1]").remove
   open(FILENAME1,"a"){|f|
-    f.write html_doc.xpath("/html/body/div/div/div[4]/div/form[2]/div[2]/div[2]/table")
+    f.write html_doc.xpath("/html/body/div/div/div[4]/div/form[2]/div[2]/div[2]/table").to_s.gsub!("\r\n","").gsub!("\n","").gsub!("\t","")
   }
   agent.page.form_with(:name => 'multiForm2'){|form|
     form.click_button(form.button_with(:name => 'fwListNaviBtnNext')) # 次へ
@@ -341,7 +341,7 @@ open(FILENAME1,"a"){|f|
 open(FILENAME1){|f|
   open(FILENAME2,"w"){|o|
     while line = f.gets
-      line.gsub!("./130050.do","https://www.hellowork.go.jp/servicef/130050.do")
+      line = line.gsub("./130050.do","https://www.hellowork.go.jp/servicef/130050.do")
       o.puts line
     end
   }
--- mkRss.rb
+++ mkRss.rb
@@ -14,6 +14,9 @@ class String
   def gsubs
     return gsub("\r\n","").gsub("\n","").gsub("\t","").gsub("<br>","")
   end
+  def dsub
+    return sub("平成","H").sub("年",".").sub("月",".").sub("日","")
+  end
 end
 
 class MkRss
@@ -26,38 +29,43 @@ class MkRss
   end
 end
 
-doc = Nokogiri::HTML(open("test.html"))
+doc = Nokogiri::HTML(open("result.html"))
 
-trs = doc.xpath("//tr")
-
-#puts trs
-
-#p trs[1].xpath("td[5]").to_html.gsub("\r\n","").gsub("\n","").gsub("\t","").gsub("<br>","").scan(/<div>.*<\/div/)
-#p gsubs(trs[1].xpath("td[5]").to_html).scan(/<div>.*<\/div/)
-#p return_between(trs[1].xpath("td[9]").to_html.gsubs,"<div>","</div>").sub("平成","H").sub("年",".").sub("月",".").sub("日","")
- 
-rss = RSS::Maker.make("1.0") {|maker|
-  xss = maker.xml_stylesheets.new_xml_stylesheet
-  xss.href = "./rss2.css"
-
-  maker.channel.about = "htt://example.com/hass/rss.xml"
-  maker.channel.title = "ハローワーク本日の新着"
-  maker.channel.description = "HelloWorkAutoSerchSystem"
-  maker.channel.link = "https://www.hellowork.go.jp/"
-  
-  maker.items.do_sort = true
-
-  id = trs[1].xpath(".//a").to_html.split("\"").last.gsub("\r\n","").gsub("\n","").gsub("\t","").sub(">","").sub("</a>","") 
-  name = trs[1].xpath("td[4]").to_html.gsub("\r\n","").gsub("\n","").gsub("\t","").sub(">","").sub("</td","").split(">").last.sub("</div","")
-  url = trs[1].xpath(".//a").to_html.split("\"")[5]
-  date = Date.parse(return_between(trs[1].xpath("td[9]").to_html.gsubs,"<div>","</div>").sub("平成","H").sub("年",".").sub("月",".").sub("日",""))
-
-  item = maker.items.new_item
-  item.title = id + " " +  name
-  item.link = url
-  item.dc_subject = name
-  item.description = return_between(trs[1].xpath("td[5]").to_html.gsubs,"<div>","</div>") + return_between(trs[1].xpath("td[6]").to_html.gsubs,"<div>","</div>") + "\n就業場所: " + return_between(trs[1].xpath("td[8]").to_html.gsubs,"<div>","</div>")
-  item.date = date.to_s
+(1..doc.xpath("//html/body/table").length).each{|i|
+  (0..doc.xpath("//html/body/table[1]/tr/td[4]").length - 1).each{|j|
+    id = doc.xpath("//html/body/table[#{i}]/tr/td[3]")[j].text
+    name = doc.xpath("//html/body/table[#{i}]/tr/td[4]")[j].text
+    url = doc.xpath("//html/body/table[#{i}]/tr/td[3]")[j].to_html.split("\"")[9]
+    point = doc.xpath("//html/body/table[#{i}]/tr/td[8]")[j].text
+    date = doc.xpath("//html/body/table[#{i}]/tr/td[9]")[j].text.dsub
+    puts id + " " + name + point + " " + date
+#    puts url
+  }
 }
+ 
 
-puts rss.to_s
+#rss = RSS::Maker.make("1.0") {|maker|
+#  xss = maker.xml_stylesheets.new_xml_stylesheet
+#  xss.href = "./rss2.css"
+
+#  maker.channel.about = "htt://example.com/hass/rss.xml"
+#  maker.channel.title = "ハローワーク本日の新着"
+#  maker.channel.description = "HelloWorkAutoSerchSystem"
+#  maker.channel.link = "https://www.hellowork.go.jp/"
+  
+#  maker.items.do_sort = true
+
+#  id = trs[1].xpath(".//a").to_html.split("\"").last.gsub("\r\n","").gsub("\n","").gsub("\t","").sub(">","").sub("</a>","") 
+#  name = trs[1].xpath("td[4]").to_html.gsub("\r\n","").gsub("\n","").gsub("\t","").sub(">","").sub("</td","").split(">").last.sub("</div","")
+#  url = trs[1].xpath(".//a").to_html.split("\"")[5]
+#  date = Date.parse(return_between(trs[1].xpath("td[9]").to_html.gsubs,"<div>","</div>").sub("平成","H").sub("年",".").sub("月",".").sub("日",""))
+
+#  item = maker.items.new_item
+#  item.title = id + " " +  name
+#  item.link = url
+#  item.dc_subject = name
+#  item.description = return_between(trs[1].xpath("td[5]").to_html.gsubs,"<div>","</div>") + return_between(trs[1].xpath("td[6]").to_html.gsubs,"<div>","</div>") + "\n就業場所: " + return_between(trs[1].xpath("td[8]").to_html.gsubs,"<div>","</div>")
+#  item.date = date.to_s
+#}
+
+#puts rss.to_s
